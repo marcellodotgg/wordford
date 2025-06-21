19c95722a7f3582dd47443375bf38d4b81157cdf
Marcello <hey@marcello.gg>
2025-06-21 00:24:07 -0400

    feat: breadcrumbs and error handling

--- src/routes/pages.rs
+++ src/routes/pages.rs
@@ -11,28 +11,37 @@ use axum::{
 };
 use std::sync::Arc;
 
-fn api_routes() -> Router<Arc<AppState>> {
-    Router::new()
-        .route("/", put(create_page))
-        .route("/{id}", get(find_page_by_id).delete(delete_page))
-        .route("/{id}/content", get(get_content_for_page))
-}
-
 pub fn routes() -> Router<Arc<AppState>> {
-    Router::new().nest("/api/pages", api_routes())
+    Router::new().nest(
+        "/pages",
+        Router::new()
+            .route("/", put(create_page))
+            .route("/{id}", get(index))
+            .route("/{id}/content", get(get_content_for_page)),
+    )
 }
 
-pub async fn find_page_by_id(
-    State(state): State<Arc<AppState>>,
-    Path(id): Path<i64>,
-) -> impl IntoResponse {
+pub async fn index(State(state): State<Arc<AppState>>, Path(id): Path<i64>) -> Html<String> {
     let page_repository = PageRepository::new(state.db.clone());
     let page_service = PageService::new(page_repository);
 
     match page_service.find_by_id(&id).await {
-        Ok(Some(page)) => Json(page).into_response(),
-        Ok(None) => StatusCode::NOT_FOUND.into_response(),
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        Ok(Some(page)) => {
+            let context = tera::Context::from_serialize(page).unwrap();
+            Html(state.tera.render("pages/index.html", &context).unwrap())
+        }
+        Ok(None) => Html(
+            state
+                .tera
+                .render("shared/404.html", &tera::Context::new())
+                .unwrap(),
+        ),
+        Err(_) => Html(
+            state
+                .tera
+                .render("shared/500.html", &tera::Context::new())
+                .unwrap(),
+        ),
     }
 }
 
@@ -85,18 +94,5 @@ pub async fn create_page(
             error_message("The provided App ID is not valid.").into_response()
         }
         Err(_) => error_message("Something went wrong creating the page.").into_response(),
-    }
-}
-
-pub async fn delete_page(
-    State(state): State<Arc<AppState>>,
-    Path(id): Path<String>,
-) -> impl IntoResponse {
-    let page_repository = PageRepository::new(state.db.clone());
-    let page_service = PageService::new(page_repository);
-
-    match page_service.delete_page(&id).await {
-        Ok(_) => StatusCode::NO_CONTENT.into_response(),
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
