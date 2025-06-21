c2272ebae61680f4c71005f3fdc30f205d3a270f
Marcello <hey@marcello.gg>
2025-06-21 13:20:06 -0400

    fix: search command and styling tweaks

--- public/ctrl_k_to_search.js
+++ public/ctrl_k_to_search.js
@@ -1,5 +1,5 @@
 document.addEventListener("keydown", function (e) {
-  if (e.key === "/") {
+  if (e.ctrlKey && e.key.toLowerCase() === "k") {
     const searchBar = document.getElementById("search_bar");
     if (searchBar && document.activeElement !== searchBar) {
       e.preventDefault();
