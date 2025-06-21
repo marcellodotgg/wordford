ed2c8c5ee7c2973e45246b5c1cb0e32f5cf982c7
Marcello <hey@marcello.gg>
2025-06-20 21:57:24 -0400

    feat: include templates when deployed

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ docker build -t wordford .
 docker run -d \
     -p 8088:3000 \
     -v $(pwd)/wordford.db:/usr/src/wordford/wordford.db \
+    -v $(pwd)/templates:/usr/src/wordford/templates \
     --name wordford_container wordford
