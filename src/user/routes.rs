a51ede5bd4f8a74fa38184f14213f11b8a04a91b
Marcello <hey@marcello.gg>
2025-06-24 20:09:16 -0400

    feat: create accounts in the browser

--- src/user/routes.rs
+++ src/user/routes.rs
@@ -6,23 +6,48 @@ use axum::{
     Form, Router,
     extract::State,
     response::{Html, IntoResponse},
-    routing::put,
+    routing::{get, put},
 };
 use std::sync::Arc;
 
 pub fn routes() -> Router<Arc<AppState>> {
-    Router::new().nest("/users", Router::new().route("/", put(create_user)))
+    Router::new()
+        .nest("/users", Router::new().route("/", put(create_user)))
+        .route("/signup", get(signup_html))
 }
 
 pub async fn create_user(
     State(state): State<Arc<AppState>>,
     Form(request): Form<CreateUserRequest>,
 ) -> impl IntoResponse {
-    let user_repository = UserRepository::new(&state.db);
-    let user_service = UserService::new(user_repository);
+    let user_service = UserService::new(UserRepository::new(&state.db));
+    let mut context = tera::Context::new();
 
-    match user_service.create_user(&request).await {
-        Ok(_) => Html("nice").into_response(),
-        Err(err) => Html(err.to_string()).into_response(),
+    let template = "user/create_user_form.html";
+    let html = match user_service.create_user(&request).await {
+        Ok(_) => {
+            context.insert("success", "Congratulations! You may now sign in.");
+            state.tera.render(template, &context)
+        }
+        Err(sqlx::Error::Database(err)) if err.is_unique_violation() => {
+            context.insert("error", "Email already exists. Please try again.");
+            state.tera.render(template, &context)
+        }
+        Err(_) => {
+            context.insert("error", "An unexpected error occurred. Please try again.");
+            state.tera.render(template, &context)
+        }
     }
+    .unwrap();
+
+    Html(html)
+}
+
+pub async fn signup_html(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    Html(
+        state
+            .tera
+            .render("user/signup.html", &tera::Context::new())
+            .unwrap(),
+    )
 }
